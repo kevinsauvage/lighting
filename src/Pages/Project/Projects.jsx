57afd7d4a06a384582012fdbd9e69ef2b0112d11
Kevin Sauvage <kevin@subforce.io>
2022-01-16 23:10:55 +0100

    add fix width and height to logo

--- src/Pages/Project/Projects.jsx
+++ src/Pages/Project/Projects.jsx
@@ -1,8 +1,10 @@
+import { useEffect } from "react";
 import { Outlet } from "react-router-dom";
 import Footer from "../../Components/Footer/Footer";
 import "./Projects.scss";
 
 const Projects = () => {
+  useEffect(() => window.scrollTo(0, 0), []);
   return (
     <div className="Projects">
       <Outlet />
